fab6cc757f38f61dbe8c7fe3e485511133bd8c80
Ge Tan <gtan@me.com>
2014-09-07 17:34:25 +0100

    add math function header

---
+++ src/MathFunctions.h
@@ -0,0 +1,11 @@
+/*************************************************************************
+    > File Name: MathFunctions.h
+    > Author: Ge Tan
+    > Mail: gtan@me.com 
+    > Created Time: Sun Sep  7 16:07:21 2014
+ ************************************************************************/
+
+#include<stdio.h>
+
+double log1x(double x);
+double exp1x(double x)
