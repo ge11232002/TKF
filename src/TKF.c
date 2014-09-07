acd83bb4ee96253407a1c4f442b02d020c19c041
Ge Tan <gtan@me.com>
2014-09-07 21:01:39 +0100

    still bug

--- src/TKF.c
+++ src/TKF.c
@@ -8,14 +8,18 @@
 #include<stdio.h>
 #include<gsl/gsl_matrix.h>
 #include<gsl/gsl_vector.h>
+#include<gsl/gsl_math.h>
+#include <gsl/gsl_min.h>
 #include "Rdefines.h"
 #include "matrix.h"
 #include "MathFunctions.h"
+
 
 struct TKF91LikelihoodFunction1D_params
 {
       double len, mu;
       gsl_matrix *substModel;
+      gsl_vector *eqFrequencies;
       gsl_vector *seq1Int, *seq2Int;
 };
 
@@ -24,6 +28,7 @@ double TKF91LikelihoodFunction1D(double distance, void *params){
   double len = p->len;
   double mu = p->mu;
   gsl_matrix *substModel = p->substModel;
+  gsl_vector *eqFrequencies = p->eqFrequencies;
   gsl_vector *seq1Int = p->seq1Int;
   gsl_vector *seq2Int = p->seq2Int;
   
@@ -36,31 +41,84 @@ double TKF91LikelihoodFunction1D(double distance, void *params){
   double lP11t = log1x(-exp(alpha) - mu * beta) + log1x(-lambda * beta);
   double lP12t = log1x(-lambda * beta); 
   double lP01t = log(mu) + lbeta;
-  
-  // test
-  int i;
-  for(i = 0; i < seq1Int->size; i++){
-    printf ("v_%d = %g\n", i, gsl_vector_get (seq1Int, i));
+ 
+  // initialize the entries tables, only log-likelihood is stored in thie table
+  int SA = seq1Int->size;
+  int SB = seq2Int->size;
+  gsl_matrix *L0 = gsl_matrix_alloc(SA+1, SB+1);
+  gsl_matrix *L1 = gsl_matrix_alloc(SA+1, SB+1);
+  gsl_matrix *L2 = gsl_matrix_alloc(SA+1, SB+1);
+
+  // initialize the boundary conditions
+  gsl_matrix_set(L0, 0, 0, -INFINITY);
+  gsl_matrix_set(L2, 0, 0, -INFINITY);
+  gsl_matrix_set(L1, 0, 0, lP12t + log1x(-lambda/mu));
+  int i, j;
+  double temp = 0;
+  for(i = 1; i <= SA; i++){
+    //for(j = 1; j <= i; j++){
+    temp = temp + log(gsl_vector_get(eqFrequencies, gsl_vector_get(seq1Int, i-1))) + lP01t;
+    gsl_matrix_set(L0, i, 0, log1x(-lambda/mu) + i * (log(lambda) - log(mu)) + lP12t + temp);
+    gsl_matrix_set(L1, i, 0, -INFINITY);
+    gsl_matrix_set(L2, i, 0, -INFINITY);
+  }
+  temp = 0;
+  for(j = 1; j <= SB; j++){
+    temp = temp + log(gsl_vector_get(eqFrequencies, gsl_vector_get(seq2Int, j-1)));
+    gsl_matrix_set(L2, 0, j, log1x(-lambda/mu) + lP12t + j * (log(lambda) + lbeta) + temp);
+    gsl_matrix_set(L1, 0, j, -INFINITY);
+    gsl_matrix_set(L0, 0, j, -INFINITY);
   }
 
-  return 0.1;
+  //recursive iteration
+  for(i = 1; i <= SA; i++){
+    for(j = 1; j <= SB; j++){
+      temp = GSL_MAX(GSL_MAX(gsl_matrix_get(L0, i-1, j),
+            gsl_matrix_get(L1, i-1, j)),
+          gsl_matrix_get(L2, i-1, j));
+      gsl_matrix_set(L0, i, j, log(lambda) - log(mu) + log(gsl_vector_get(eqFrequencies, gsl_vector_get(seq1Int, i-1))) + lP01t + temp + log(exp(gsl_matrix_get(L0, i-1, j) - temp) + exp(gsl_matrix_get(L1, i-1, j) - temp) + exp(gsl_matrix_get(L2, i-1, j) - temp)));
+      temp = GSL_MAX(GSL_MAX(gsl_matrix_get(L0, i-1, j-1), gsl_matrix_get(L1, i-1, j-1)), gsl_matrix_get(L2, i-1, j-1));
+      gsl_matrix_set(L1, i, j, log(lambda) - log(mu) + log(gsl_vector_get(eqFrequencies, gsl_vector_get(seq1Int, i-1))) + log(gsl_matrix_get(substModel, gsl_vector_get(seq1Int, i-1), gsl_vector_get(seq2Int, j-1)) * exp(lP1t) + gsl_vector_get(eqFrequencies, gsl_vector_get(seq2Int, j-1) * exp(lP11t))) + temp + log(exp(gsl_matrix_get(L0, i-1, j-1) - temp) + exp(gsl_matrix_get(L1, i-1, j-1) - temp) + exp(gsl_matrix_get(L2, i-1, j-1) - temp)));
+      if(isfinite(gsl_matrix_get(L1, i, j-1)) || isfinite(gsl_matrix_get(L2, i, j-1))){
+        temp = GSL_MAX(gsl_matrix_get(L1, i, j-1), gsl_matrix_get(L2, i,j-1));
+        gsl_matrix_set(L2, i, j, log(gsl_vector_get(eqFrequencies, gsl_vector_get(seq2Int, j-1))) + log(lambda) + lbeta + temp + log(exp(gsl_matrix_get(L1, i, j-1) - temp) + exp(gsl_matrix_get(L2, i, j-1) - temp)));
+      }else{
+        gsl_matrix_set(L2, i, j, -INFINITY);
+      }
+    }
+  }
+  temp = GSL_MAX(GSL_MAX(gsl_matrix_get(L0, SA, SB), gsl_matrix_get(L1, SA, SB)), gsl_matrix_get(L2, SA, SB));
+  double likelihood;
+  likelihood = -(temp + log(exp(gsl_matrix_get(L0, SA, SB) - temp) + exp(gsl_matrix_get(L1, SA, SB) - temp) + exp(gsl_matrix_get(L2, SA, SB) - temp)));
+  // free the allocated matrix
+  gsl_matrix_free(L0);
+  gsl_matrix_free(L1);
+  gsl_matrix_free(L2);
+
+  return likelihood;
 }
 
 SEXP TKF91LikelihoodFunction1DMain(SEXP seq1Int, SEXP seq2Int, SEXP mu,
-    SEXP expectedLength, SEXP probMatR){
+    SEXP expectedLength, SEXP probMatR, SEXP eqFrequenciesR){
   int ncol, nrow;
   ncol = INTEGER(GET_DIM(probMatR))[1];
   nrow = INTEGER(GET_DIM(probMatR))[0];
   //double distance;
   //distance = REAL(distanceR)[0];
   int i, j; 
-  // matrix allocation and setting
+  
+  // probMat
   gsl_matrix *probMat = gsl_matrix_alloc(nrow, ncol);
   //gsl_matrix *probMatN = gsl_matrix_alloc(nrow, ncol);
   for(i = 0; i < nrow; i++) 
     for(j = 0; j < ncol; j++)
       gsl_matrix_set(probMat, i, j, REAL(probMatR)[i+j*ncol]);
- 
+  // eqFrequenciesR
+  gsl_vector *eqFrequencies = gsl_vector_alloc(GET_LENGTH(eqFrequenciesR));
+  for(i = 0; i < GET_LENGTH(eqFrequenciesR); i++){
+    gsl_vector_set(eqFrequencies, i, REAL(eqFrequenciesR)[i]);
+  }
+
   // seq preparation
   gsl_vector *seq1IntGSL = gsl_vector_alloc(GET_LENGTH(seq1Int));
   gsl_vector *seq2IntGSL = gsl_vector_alloc(GET_LENGTH(seq2Int));
@@ -70,9 +128,10 @@ SEXP TKF91LikelihoodFunction1DMain(SEXP seq1Int, SEXP seq2Int, SEXP mu,
   for(i = 0; i < GET_LENGTH(seq2Int); i++){
     gsl_vector_set(seq2IntGSL, i, INTEGER(seq2Int)[i]);
   }
-  for(i = 0; i < seq1IntGSL->size; i++){
-    printf ("v_%d = %g\n", i, gsl_vector_get (seq1IntGSL, i));
-  }
+
+  //for(i = 0; i < seq1IntGSL->size; i++){
+  // printf ("v_%d = %g\n", i, gsl_vector_get (seq1IntGSL, i));
+  //}
   // GSL minimizer 
   /*int status;
   int iter = 0, max_iter = 100;
@@ -83,6 +142,7 @@ SEXP TKF91LikelihoodFunction1DMain(SEXP seq1Int, SEXP seq2Int, SEXP mu,
   params->len = REAL(expectedLength)[0];
   params->mu = REAL(mu)[0];
   params->substModel = probMat;
+  params->eqFrequencies = eqFrequencies;
   params->seq1Int = seq1IntGSL;
   params->seq2Int = seq2IntGSL;
   F.function = &TKF91LikelihoodFunction1D;
@@ -113,19 +173,19 @@ SEXP TKF91LikelihoodFunction1DMain(SEXP seq1Int, SEXP seq2Int, SEXP mu,
       printf ("%5d [%.7f, %.7f] "
               "%.7f %.7f\n",
               iter, x_lo,  x_hi,
-              x, b - a);
+              x, x_hi - x_lo);
     }
   while (status == GSL_CONTINUE && iter < max_iter);
-*/
+ */ 
   //PAMn(probMat, distance, probMatN); 
   //printGSLMatrix(probMatN);
   
-  // free the GSL matrix
+  // free the allocation
   gsl_matrix_free(probMat);
-  //gsl_min_fminimizer_free (s);
+  gsl_min_fminimizer_free (s);
   gsl_vector_free(seq1IntGSL);
   gsl_vector_free(seq2IntGSL);
-  //gsl_matrix_free(probMatN);
+  gsl_vector_free(eqFrequencies);
 
   return R_NilValue;
 }
