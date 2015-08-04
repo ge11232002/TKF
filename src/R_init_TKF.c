1113664a5026f00fecb6e5ff3bff77f1bad3c381
Ge Tan <gtan@me.com>
2015-08-04 16:34:31 +0100

    version 0.1.0. Using nlopt for optimisation

--- src/R_init_TKF.c
+++ src/R_init_TKF.c
@@ -1,6 +1,9 @@
 #include "TKF91.h"
+#include "TKF91_nlopt.h"
 #include "TKF92.h"
+#include "TKF92_nlopt.h"
 #include "TKF92HG.h"
+#include "TKF92HG_nlopt.h"
 
 #define CALLMETHOD_DEF(fun, numArgs) {#fun, (DL_FUNC) &fun, numArgs}
 
@@ -9,16 +12,20 @@ static const R_CallMethodDef callMethods[] = {
   CALLMETHOD_DEF(TKF91LikelihoodFunctionWrapper, 7),
   CALLMETHOD_DEF(TKF91LikelihoodFunction1DMain, 6),
   CALLMETHOD_DEF(TKF91LikelihoodFunction2DMainNM, 5),
+  CALLMETHOD_DEF(TKF91LikelihoodFunction2DMain_nlopt, 6),
 
   /* TKF92.c */
   CALLMETHOD_DEF(TKF92LikelihoodFunctionWrapper, 8),
   CALLMETHOD_DEF(TKF92LikelihoodFunction1DMain, 7),
   CALLMETHOD_DEF(TKF92LikelihoodFunction3DMainNM, 5),
+  CALLMETHOD_DEF(TKF92LikelihoodFunction3DMain_nlopt, 6),
 
   /* TKF92HG.c */
   CALLMETHOD_DEF(TKF92HGLikelihoodFunctionWrapper, 10),
   CALLMETHOD_DEF(TKF92HGLikelihoodFunction1DMain, 9),
   CALLMETHOD_DEF(TKF92HGLikelihoodFunction5DMainNM, 5),
+  CALLMETHOD_DEF(TKF92HGLikelihoodFunction5DMain_nlopt, 6),
+
   {NULL, NULL, 0}
 };
 
